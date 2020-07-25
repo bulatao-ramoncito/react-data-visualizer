ef73025334dc99af772da72d891c44f9f8360d5d
Monchit Bulatao <ramoncitobulatao@gmail.com>
2020-07-25 23:06:38 +0800

    Add Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import "./App.css";
 import Body from "./components/Body";
+import NavBar from "./components/layout/NavBar";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Body />
     </div>
   );
